b794fe7b973d4eb65c393b70e153e13b3104ed69
Adarsh-Kant <adarshkant0@gmail.com>
2025-10-09 07:13:42 +0530

    Update SocketContext.jsx

--- Backend/Frontend/src/context/SocketContext.jsx
+++ Backend/Frontend/src/context/SocketContext.jsx
@@ -67,7 +67,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5002/", {
+      const socket = io("https://chatsphere-2-cztb.onrender.com", {
         query: { userId: authUser.user._id },
       });
       setSocket(socket);
